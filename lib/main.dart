1c121b5c7b44b59db963b55206397d86e4cc23fb
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-09-03 09:09:53 +0700

    langkah 4 - use an external package

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: const Text('Welcome to Flutter'),
         ),
         body: Center(
           // child: Text('Hello World')
