55373032753ac5d5ff41116a5d28ab4cc8ba21ab
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-09-03 09:11:07 +0700

    langkah 5 - add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
+        body: const Center(
           // child: Text('Hello World')
-          child: Text(wordPair.asPascalCase),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
